3dba73cd32a8a541c4d8b9dc83fecd4a9c7fb80b
Felipe Rangel <78965577+felipesrangel@users.noreply.github.com>
2023-04-15 13:11:03 -0300

    criando select e option

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
+
+  public listComidas: Array<{comida: string, preco:string}> = [
+    {comida: "X-salada", preco: "R$10,00"},
+    {comida: "X-bacon", preco: "R$11,00"},
+    {comida: "Coxinha", preco: "R$12,00"},
+  ];
+
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
 
 }
